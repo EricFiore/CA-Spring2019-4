2511fa55ff0af2de30eca1a75ff41265dcb179ef
ericfiore <eric.fiore@live.com>
2019-02-12 10:23:37 -0500

    enlosed structure and starting animation of obstacles

---
+++ Assets/Scripts/Navigation/TranslateLaterally.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateLaterally : MonoBehaviour
+{
+    public float speed;
+    private Vector3 startPoint = new Vector3(20, 1, 63);
+    public Vector3 endPoint = new Vector3(-20, 1, 68);
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(startPoint, endPoint, Mathf.PingPong(Time.time, 1));
+    }
+}
